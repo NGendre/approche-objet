c0bf682947149c5bbbe898535174a97cbabcf874
Noe Gendre <noe.gendre@outlook.fr>
2021-10-12 16:08:03 +0200

    environnement de test et creation de personnage

---
+++ src/fr/diginamic/essais/TestJdr.java
@@ -0,0 +1,13 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.jdr.Personnage;
+
+public class TestJdr {
+
+	public static void main(String[] args) {
+		Personnage p = new Personnage();
+		System.out.println(p);
+
+	}
+
+}
--- src/fr/diginamic/jdr/Personnage.java
+++ src/fr/diginamic/jdr/Personnage.java
@@ -1,5 +1,30 @@
 package fr.diginamic.jdr;
 
-public class Personnage {
+import java.util.Random;
 
+public class Personnage {
+	private int force;
+	private int pointsDeVie;
+	private int score;
+	
+	
+	public Personnage() {
+		Random r = new Random();
+		Random r2  = new Random();
+		int force = r.nextInt(18-12)+12;
+		int pointsDeVie = r2.nextInt(50-20)+20;
+		this.force = force;
+		this.pointsDeVie = pointsDeVie;
+		this.score = 0;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Personnage [force=" + force + ", pointsDeVie=" + pointsDeVie + ", score=" + score + "]";
+	}
+	
+	
+	
+	
 }
